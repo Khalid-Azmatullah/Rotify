2b98bc6bbce9cef6e41b418f0a1bbe2d129a6b42
Khalid-Azmatullah <khalid.studymaterial@gmail.com>
2025-05-06 01:37:28 +0530

    ea1

--- downloader.py
+++ downloader.py
@@ -2,7 +2,15 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 import os
 
-urls = ["https://youtu.be/XTp5jaRU3Ws?si=7L9Agj05RsK5fjfF"]
+urls = ["https://youtu.be/8of5w7RgcTc?si=pZjOHZ_d_S25mQEz", 
+        "https://youtu.be/Y2lWEVyO-qE?si=HAJoclIRpBd2ERD2", 
+        "https://youtu.be/NwdQx2P_ytk?si=9es2RcKDFF02Q3-b", 
+        "https://youtu.be/vX2cDW8LUWk?si=lZsSYwGBcg4J2xYZ", 
+        "https://youtu.be/0pWsCiBvLOk?si=T7nkRhkd-uCx9GBn", 
+        "https://youtu.be/-2RAq5o5pwc?si=thQk8MKQvDIjUeFc", 
+        "https://youtu.be/AKH6ZNSnWOA?si=KeCHfK_Ob3BAwQTB", 
+        "https://youtu.be/4tywp83zkmk?si=tKH2b6ovj1G7tDoa"
+    ]
 
 for url in urls:
     yt = YouTube(url, on_progress_callback=on_progress)
