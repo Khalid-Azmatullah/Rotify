cf9a04cdd8df5c2b8019b79cd7f54eaf7ed5e432
Khalid-Azmatullah <khalid.studymaterial@gmail.com>
2025-05-06 22:57:32 +0530

    ea3.4

--- downloader.py
+++ downloader.py
@@ -3,28 +3,28 @@ from pytubefix.cli import on_progress
 from pytubefix import Playlist
 import os
 
-# urls = ["https://youtu.be/8of5w7RgcTc?si=pZjOHZ_d_S25mQEz", 
-#         "https://youtu.be/Y2lWEVyO-qE?si=HAJoclIRpBd2ERD2", 
-#         "https://youtu.be/NwdQx2P_ytk?si=9es2RcKDFF02Q3-b", 
-#         "https://youtu.be/vX2cDW8LUWk?si=lZsSYwGBcg4J2xYZ", 
-#         "https://youtu.be/0pWsCiBvLOk?si=T7nkRhkd-uCx9GBn", 
-#         "https://youtu.be/-2RAq5o5pwc?si=thQk8MKQvDIjUeFc", 
-#         "https://youtu.be/AKH6ZNSnWOA?si=KeCHfK_Ob3BAwQTB", 
-#         "https://youtu.be/4tywp83zkmk?si=tKH2b6ovj1G7tDoa"
-    # ]
-urls = []
+urls = ["https://youtu.be/8of5w7RgcTc?si=pZjOHZ_d_S25mQEz", 
+        "https://youtu.be/Y2lWEVyO-qE?si=HAJoclIRpBd2ERD2", 
+        "https://youtu.be/NwdQx2P_ytk?si=9es2RcKDFF02Q3-b", 
+        "https://youtu.be/vX2cDW8LUWk?si=lZsSYwGBcg4J2xYZ", 
+        "https://youtu.be/0pWsCiBvLOk?si=T7nkRhkd-uCx9GBn", 
+        "https://youtu.be/-2RAq5o5pwc?si=thQk8MKQvDIjUeFc", 
+        "https://youtu.be/AKH6ZNSnWOA?si=KeCHfK_Ob3BAwQTB", 
+        "https://youtu.be/4tywp83zkmk?si=tKH2b6ovj1G7tDoa",
+        "https://youtu.be/XTp5jaRU3Ws?si=7L9Agj05RsK5fjfF"  
+    ]
 
 
-playlist_url = "https://youtube.com/playlist?list=PLttkmJc3m9Muf1yJ_1nGqCxBrhEJZPvFh&si=pk4kw5G099uPSCOg"
+# playlist_url = "https://youtube.com/playlist?list=PLttkmJc3m9Muf1yJ_1nGqCxBrhEJZPvFh&si=pk4kw5G099uPSCOg"
 
-# Initialize the Playlist object
-playlist = Playlist(playlist_url)
+# # Initialize the Playlist object
+# playlist = Playlist(playlist_url)
 
-# Download audio from each video in the playlist
-for video in playlist.videos:
-    video.register_on_progress_callback(on_progress)
-    audio_stream = video.streams.filter(only_audio=True).order_by('abr').desc().first()
-    audio_stream.download(output_path="Songs")
+# # Download audio from each video in the playlist
+# for video in playlist.videos:
+#     video.register_on_progress_callback(on_progress)
+#     audio_stream = video.streams.filter(only_audio=True).order_by('abr').desc().first()
+#     audio_stream.download(output_path="Songs")
 
 
 
